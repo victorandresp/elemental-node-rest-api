afed0f22c410e1849c83cf73ea2ce2c5421be466
victorandresp <ponceottamendi27@gmail.com>
2023-05-30 00:26:05 -0400

    chore: remove console.logs :muted:

--- quotes/controllers/quote.controller.js
+++ quotes/controllers/quote.controller.js
@@ -5,12 +5,10 @@ const fs = require("fs")
 
 class QuoteController {
     async get(req, res){
-        console.log('calleddd');
         return res.status(200).send(db)
     }
     async getOne(req, res){
         const { id } = req.params
-        console.log('id', typeof id);
         const quoteGetted = db.find(q => q.id === parseInt(id))
         if(!quoteGetted){
             return res.status(404).send({ message: 'Quote not found !'})
@@ -20,7 +18,6 @@ class QuoteController {
     async add(req, res){
         try {
             const { body: quote } = req
-            console.log(quote);
             const id = db.length
             quote.id = id
             db.push(quote)
@@ -28,7 +25,6 @@ class QuoteController {
             return res.status(201).send({ message: `Added new quote with the id: ${id}`})
             
         } catch (error) {
-            console.log(error);
             return res.status(400).send({ message: 'Bad request !'})
         }
     }
