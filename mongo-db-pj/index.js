ee2e76de692c9520e2a718ab9225de24a601e08c
victorandresp <ponceottamendi27@gmail.com>
2023-09-10 22:59:20 -0400

    feat: save on data base and added cronjob time :sparkles:

--- mongo-db-pj/index.js
+++ mongo-db-pj/index.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGO_ATLAS_URI, { useNewUrlParser: true}
     ).then(() => {
         console.log("mongodb is connected");
-        getBreakingNew();
-        // const Cat = mongoose.model("Cat", {name: String })
-        // Cat.find().then(console.log)
+        getBreakingNew(); // CronJob for obtain notices by Scrapping
+        const Cat = mongoose.model("Cat", {name: String })
+        Cat.find().then(console.log) 
     })
 
--- mongo-db-pj/jobs/breakingNews.job.js
+++ mongo-db-pj/jobs/breakingNews.job.js
@@ -1,25 +1,32 @@
-const mongoose = require("mongoose");
 const axios = require("axios").default
 const cheerio = require("cheerio")
 const cron = require("node-cron")
 
-const getBreakingNew = async () => {
-    try {
-        const htmlNew = await axios.get("https://cnnespanol.cnn.com/")
-        const $ = cheerio.load(htmlNew.data)
-        const titles = $(".news__title")
-        titles.each((index, element) => {
-            const breakingNew = { 
-                title: $(element).text().trim(),
-                link: $(element).children().attr("href"),
+const { BreakingNew } = require("../models")
 
-            }
-            console.log('breakingNew', breakingNew);
-        })
-        
-    } catch (error) {
-        console.error(error)
-    }
+const getBreakingNew = () => {
+    cron.schedule("* * * * *", async () => {
+        try {
+            console.log('CronJob Executed !!');
+            const htmlNew = await axios.get("https://cnnespanol.cnn.com/")
+            const $ = cheerio.load(htmlNew.data)
+            const titles = $(".news__title")
+            const news = []
+            titles.each((index, element) => {
+                const breakingNew = { 
+                    title: $(element).text().trim(),
+                    link: $(element).children().attr("href"),
+                }
+                news.push(breakingNew);
+            })
+
+            await BreakingNew.create(news)
+             
+        } catch (error) {
+            console.error(error)
+        }
+
+    })
 }
 
 module.exports = {
