0e02d6e26047bd42a52d83788238eda9ca086e69
victorandresp <ponceottamendi27@gmail.com>
2023-09-19 23:03:01 -0400

    feat: injected into homecontroller the homeservice and registered in container :sparkles:

--- RESTful/src/startup/container.js
+++ RESTful/src/startup/container.js
@@ -3,10 +3,16 @@ const { createContainer, asClass, asValue, asFunction } = require("awilix");
 //services
 const { HomeService } = require("../services")
 
+// controllers 
+const { HomeController } = require("../controllers")
+
 const container = createContainer();
 
 container.register({
     HomeService: asClass(HomeService).singleton()
+})
+.register({
+    HomeController: asClass(HomeService).bind(HomeController).singleton()
 })
 
 module.exports = container
