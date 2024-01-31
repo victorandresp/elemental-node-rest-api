44e898c500ec61b9c3a06ea9d310c3d9eb04f42a
victorandresp <ponceottamendi27@gmail.com>
2024-01-30 23:31:59 -0400

    fix: error with mongodb config :bug:

--- ideas-api/index.js
+++ ideas-api/index.js
@@ -4,8 +4,7 @@ const server = container.resolve("app")
 const { MONGO_URI } = container.resolve("config")
 
 const mongoose = require("mongoose")
-mongoose.set("useCreateIndex", true)
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false})
+mongoose.connect(MONGO_URI, { useNewUrlParser: true})
         .then(() => server.start())
         .catch(console.error)
