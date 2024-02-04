9a81ca01344b35f386d740022d08718a21ea8828
victorandresp <ponceottamendi27@gmail.com>
2024-02-03 23:50:58 -0400

    feat:idea schema :sparkles:

---
+++ ideas-api/src/models/idea.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const { Schema } = mongoose
+
+const IdeaSchema = new Schema ({
+    idea: { type: String, required: true },
+    description: { type: String  },
+    upvotes: [{type: Boolean}],
+    downvotes:[{type: Boolean}],
+    author: {
+        type: Schema.Types.ObjectId, 
+        ref: "user", 
+        required:true, 
+        autopopulate: true
+    }
+})
+
+module.exports = mongoose.model("idae", IdeaSchema)
