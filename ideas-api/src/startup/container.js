3da0598cf706f98313da2805675ac5873abd48fb
victorandresp <ponceottamendi27@gmail.com>
2024-02-07 23:04:00 -0400

    chore: injected services :sparkles:

--- ideas-api/src/startup/container.js
+++ ideas-api/src/startup/container.js
@@ -6,7 +6,7 @@ const config = require("../config")
 const app = require(".")
 
 //services
-const { HomeService } = require("../services");
+const { HomeService, UserService, IdeaService, CommentService } = require("../services");
 
 //controllers
 const { HomeController } = require("../controllers");
@@ -31,7 +31,10 @@ container
         config: asValue(config)
     })
     .register({
-        HomeService: asClass(HomeService).singleton()
+        HomeService: asClass(HomeService).singleton(),
+        UserService: asClass(UserService).singleton(),
+        IdeaService: asClass(IdeaService).singleton(),
+        CommentService: asClass(CommentService).singleton(),
     })
     .register({
         HomeController: asClass(HomeController.bind(HomeController)).singleton()
