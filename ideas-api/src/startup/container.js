f3ffe48c7dc67371b95a81d740cd62097e17a98f
victorandresp <ponceottamendi27@gmail.com>
2024-02-09 00:02:00 -0400

    feat:injected controllers into container :sparkles:

--- ideas-api/src/startup/container.js
+++ ideas-api/src/startup/container.js
@@ -9,7 +9,7 @@ const app = require(".")
 const { HomeService, UserService, IdeaService, CommentService } = require("../services");
 
 //controllers
-const { HomeController } = require("../controllers");
+const { HomeController, UserController, IdeaController, CommentController } = require("../controllers");
 
 //routes
 const { HomeRoutes } = require("../routes/index.routes");
@@ -37,7 +37,10 @@ container
         CommentService: asClass(CommentService).singleton(),
     })
     .register({
-        HomeController: asClass(HomeController.bind(HomeController)).singleton()
+        HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+        UserController: asClass(UserController.bind(UserController)).singleton(),
+        IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
+        CommentController: asClass(CommentController.bind(CommentController)).singleton(),
     })
     .register({
         HomeRoutes: asFunction(HomeRoutes).singleton()
