213128523976746df113cef7faa8417d42e42c0b
victorandresp <ponceottamendi27@gmail.com>
2024-02-12 23:59:57 -0400

    feat: injected auth routes, controller and services :sparkles:

--- ideas-api/src/startup/container.js
+++ ideas-api/src/startup/container.js
@@ -6,13 +6,13 @@ const config = require("../config")
 const app = require(".")
 
 //services
-const { HomeService, UserService, IdeaService, CommentService } = require("../services");
+const { HomeService, UserService, IdeaService, CommentService, AuthService } = require("../services");
 
 //controllers
-const { HomeController, UserController, IdeaController, CommentController } = require("../controllers");
+const { HomeController, UserController, IdeaController, CommentController, AuthController } = require("../controllers");
 
 //routes
-const { HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes } = require("../routes/index.routes");
+const { HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes, AuthRoutes } = require("../routes/index.routes");
 const Routes = require("../routes")
 
 //models
@@ -35,18 +35,21 @@ container
         UserService: asClass(UserService).singleton(),
         IdeaService: asClass(IdeaService).singleton(),
         CommentService: asClass(CommentService).singleton(),
+        AuthService: asClass(AuthService).singleton(),
     })
     .register({
         HomeController: asClass(HomeController.bind(HomeController)).singleton(),
         UserController: asClass(UserController.bind(UserController)).singleton(),
         IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
         CommentController: asClass(CommentController.bind(CommentController)).singleton(),
+        AuthController: asClass(AuthController.bind(AuthController)).singleton(),
     })
     .register({
         HomeRoutes: asFunction(HomeRoutes).singleton(),
         UserRoutes: asFunction(UserRoutes).singleton(),
         IdeaRoutes: asFunction(IdeaRoutes).singleton(),
         CommentRoutes: asFunction(CommentRoutes).singleton(),
+        AuthRoutes: asFunction(AuthRoutes).singleton(),
     })
     .register({
         User: asValue(User),
