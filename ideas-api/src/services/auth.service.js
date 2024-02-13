960c74fa91ea2e2c346e456d0dcc1c9643d3589e
victorandresp <ponceottamendi27@gmail.com>
2024-02-12 23:50:18 -0400

    feat: signIn function for auth.service :sparkles:

--- ideas-api/src/services/auth.service.js
+++ ideas-api/src/services/auth.service.js
@@ -15,6 +15,27 @@ class AuthService extends BaseService {
         }
         return await _userService.create(user)
     }
+
+    async signIn(user){
+        const { username, password } = user
+
+        const userExists = await _userService.getUserByUsername(username)
+        if(!userExists){
+            ThrowHttpError(404, "user doesn't exists")
+        }
+        const validPassword = userExists.comparePasswords(password) // this function can be called because userExists is a mongoose entity of User Model.
+        if(!validPassword){
+            ThrowHttpError(401, "invalid password")
+        }
+
+        const userToEncode = {
+            username: userExists.username,
+            id: userExists._id
+        }
+        const token = JWTHelper(userToEncode)
+
+        return { token, user: userExists }
+    }
 }
 
 module.exports = AuthService
