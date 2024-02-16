b2c31b8171a1716d3ff35b9aae172a7b55ff4b6b
victorandresp <ponceottamendi27@gmail.com>
2024-02-16 17:14:31 -0400

    fix: changed return function of request and added argument by duration :bug:

--- ideas-api/src/helpers/index.js
+++ ideas-api/src/helpers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     ThrowHttpError: require("./error.helper"),
     JWTHelper: require("./jwt.helper"),
+    CacheTime: require("./cache-time.helper"),
 }
--- ideas-api/src/middlewares/cache.middleware.js
+++ ideas-api/src/middlewares/cache.middleware.js
@@ -1,18 +1,20 @@
 const mcache = require("memory-cache")
 const { CACHE_KEY } = require("../config")
 
-module.exports = function(req, res, next){
-    const key = CACHE_KEY + req.originUrl || req.originUrl
-    const cachedBody = mcache.get(cachedBody)
+module.exports = function(duration){
+    return (req, res, next) => {
+        const key = CACHE_KEY + req.originUrl || req.originUrl
+        const cachedBody = mcache.get(key)
 
-    if(cachedBody){
-        res.send(JSON.parse(cachedBody))
-    }else{
-        res.sendResponse = res.send
-        res.send = body => {
-            mcache.put(key, body, duration * 1000)
-            res.sendResponse(body);
+        if(cachedBody){
+            res.send(JSON.parse(cachedBody))
+        }else{
+            res.sendResponse = res.send
+            res.send = body => {
+                mcache.put(key, body, duration * 1000)
+                res.sendResponse(body);
+            }
+            next()
         }
-        next()
     }
 }
