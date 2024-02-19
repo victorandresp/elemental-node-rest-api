3ae1a207c3136df8a7dda28c4da24be55b4558c3
victorandresp <ponceottamendi27@gmail.com>
2024-02-18 23:44:23 -0400

    feat: added mocks for User Model and User Repository :clown_face:

---
+++ ideas-api/tests/mocks/user/user.model.mock.js
@@ -0,0 +1,23 @@
+module.exports = {
+    user:{
+        "_id": "65ced666529e74db5d57b39b",
+        "name": "Victor4",
+        "username": "victorandres.dev4",
+        "password": "test123*"
+
+    },
+    users:[
+        {
+            "_id": "65ced666529e74db5d57b39b",
+            "name": "Victor4",
+            "username": "victorandres.dev4",
+            "password": "test123*"
+        },
+        {
+            "_id": "65ced6b4529e74db5d57b39e",
+            "name": "Victor5",
+            "username": "victorandres.dev5",
+            "password": "test123*"
+        }
+    ]
+}
