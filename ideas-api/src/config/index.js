1b309e19224186654993dcbd3310a879769907ca
victorandresp <ponceottamendi27@gmail.com>
2024-02-22 23:37:51 -0400

    chore: swagger ENV variable :wrench:

--- ideas-api/src/config/index.js
+++ ideas-api/src/config/index.js
@@ -8,5 +8,6 @@ module.exports = {
     APPLICATION_NAME: process.env.APPLICATION_NAME,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-    CACHE_KEY: process.env.CACHE_KEY
+    CACHE_KEY: process.env.CACHE_KEY,
+    SWAGGER_PATH: `../config/swagger/${process.env.SWAGGER_DOC}.json`
 }
